02bad4afe573a6b174313dfe35f06ebf14bc0027
Fatiha qayouh <109628765+FatihaQAYOUH@users.noreply.github.com>
2025-12-30 01:11:15 +0100

    Update app_routes.dart

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -5,7 +5,10 @@ import '../presentation/program_summary/program_summary.dart';
 import '../presentation/authentication_screen/authentication_screen.dart';
 import '../presentation/session_player/session_player.dart';
 import '../presentation/post_session_feedback/post_session_feedback.dart';
-
+import '../presentation/ipaq_assessment/ipaq_assessment_screen.dart';
+import '../presentation/mobility_assessment/mobility_assessment_screen.dart';
+import '../presentation/stability_assessment/stability_assessment_screen.dart';
+import '../presentation/onboarding_guide/onboarding_guide_screen.dart';
 class AppRoutes {
   // TODO: Add your routes here
   static const String initial = '/';
@@ -13,8 +16,12 @@ class AppRoutes {
   static const String redFlagsAlert = '/red-flags-alert';
   static const String programSummary = '/program-summary';
   static const String authentication = '/authentication-screen';
+  static const String onboardingGuide = '/onboarding_guide';
   static const String sessionPlayer = '/session-player';
   static const String postSessionFeedback = '/post-session-feedback';
+  static const String ipaqAssessment = '/ipaq_assessment';
+  static const String mobilityAssessment = '/mobility-assessment';
+  static const String stabilityAssessment = '/stability-assessment';
 
   static Map<String, WidgetBuilder> routes = {
     initial: (context) => const AuthenticationScreen(),
@@ -24,6 +31,11 @@ class AppRoutes {
     authentication: (context) => const AuthenticationScreen(),
     sessionPlayer: (context) => const SessionPlayer(),
     postSessionFeedback: (context) => const PostSessionFeedback(),
+    ipaqAssessment: (context) => const IpaqAssessmentScreen(),
+    mobilityAssessment: (context) => const MobilityAssessmentScreen(),
+    stabilityAssessment: (context) => const StabilityAssessmentScreen(),
+    onboardingGuide: (context) => const OnboardingGuideScreen(),
+    
     // TODO: Add your other routes here
   };
 }
